52042d9fda429794ade1b3f834fe543468c3833f
ohangl <angelachacongonzalez@gmail.com>
2024-11-13 03:29:49 -0300

    C#2 finishedW01

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Tell me your Grade Percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter =  "B";
+        }
+        else if (percent >=70)
+        {
+            letter = "c";
+        }
+        else if (percent >=60)
+        {
+            letter = "D";
+        }
+        else {
+            letter = "F";
+        }
+    
+        Console.WriteLine($"Your grade is {letter}");
+    
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
